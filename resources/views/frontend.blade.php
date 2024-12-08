4ac1607887295a73818cb14d7d2469b82396abae
Max-van-Dongen <maximomus+1@gmail.com>
2024-12-08 16:13:24 +0100

    Upgrade packages

--- resources/views/frontend.blade.php
+++ resources/views/frontend.blade.php
@@ -4,9 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DocGov</title>
-    <!-- MDB5 CSS -->
+    <!-- Font Awesome -->
     <link
-        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+        rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+        href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.1.0/mdb.min.css"
         rel="stylesheet"
     />
     <!-- Tabler Icons -->
@@ -103,9 +113,10 @@
 
 
 <!-- MDB5 JS -->
+
 <script
     type="text/javascript"
-    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.0.0/mdb.min.js"
+    src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/8.1.0/mdb.umd.min.js"
 ></script>
 </body>
 </html>
